cb888fc314863a0bdfd3aaa1a0e3edbaeccde214
Jose Talens <jose.talens@williamhill.com>
2016-02-25 16:29:30 +0000

    We now get a single file in a commit

--- src/main/scala/ziggurat/GitProcess.scala
+++ src/main/scala/ziggurat/GitProcess.scala
@@ -4,8 +4,14 @@ import java.io.File
 import sys.process._
 
 case class GitProcess(gitDir: File) {
+  val absPath = gitDir.getAbsolutePath
   final val commitList = "git --git-dir=%s log --reverse --topo-order --no-merges --format=%%H#%%cn#%%cd"
+  final val fileFromCommit = "git --git-dir=%s show %s --name-only --oneline"
+
+  def filesFromCommits(commitHash: String) : Iterator[String] = {
+    Process(fileFromCommit.format(absPath, commitHash)).lineStream.to[Iterator].drop(1)
+  }
   def commits : Iterator[String] = {
-    Process(commitList.format(gitDir.getAbsolutePath)).lineStream.to[Iterator]
+    Process(commitList.format(absPath)).lineStream.to[Iterator]
   }
 }
--- src/test/scala/ziggurat/GitProcessSpec.scala
+++ src/test/scala/ziggurat/GitProcessSpec.scala
@@ -5,18 +5,23 @@ import java.io.File
 import org.specs2.Specification
 
 
-class GitProcessSpec extends Specification{
-  def is = s2"""
-          Get commit list from a repository $commitListFromGitRepo
+class GitProcessSpec extends Specification {
+  def is =
+    s2"""
+          Get commit list from a repository   $commitListFromGitRepo
+          Get a file list from a commit hash  $fileListFromCommit
     """
 
   def commitListFromGitRepo = {
-    val path: String = "./src/test/resources/gitSampleRepo/.git"
-    val gitDir: File = new File(path)
+    val gitDir: File = new File("./src/test/resources/gitSampleRepo/.git")
     val gitProc = new GitProcess(gitDir)
     gitProc.commits must_== List("264ac213c1ecf105e39c6e39aed93358ee0620c6#Jose Talens#Thu Feb 25 09:37:12 2016 +0000")
   }
 
-
+  def fileListFromCommit = {
+    val gitDir: File = new File("./src/test/resources/gitSampleRepo/.git")
+    val gitProc = new GitProcess(gitDir)
+    gitProc.filesFromCommits("264ac213c1ecf105e39c6e39aed93358ee0620c6") must_== List("README.md")
+  }
 
 }
