e15c2795b595ba468c6fe4c93b9797cebe5e2f9f
Jose Talens <jltalens@hotmail.com>
2016-04-03 15:41:19 +0100

    git directory is optional

--- src/main/scala/ziggurat/Directory.scala
+++ src/main/scala/ziggurat/Directory.scala
@@ -4,7 +4,7 @@ import java.io.File
 
 trait Directory {
 
-  def fromString(pathAsString: String, gitDir: String): Option[Repository] = {
+  def fromString(pathAsString: String, gitDir: String = ".git"): Option[Repository] = {
     val path = new File(pathAsString)
     path.isDirectory && path.list.contains(gitDir) match {
       case false => None
