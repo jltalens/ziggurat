c61ef2f1be5d299f339f1e9637243477812f840a
Jose Talens <jose.talens@williamhill.com>
2016-04-04 09:15:47 +0100

    We can now retrieve the content of a file for a specific commit

--- src/main/scala/ziggurat/GitProcess.scala
+++ src/main/scala/ziggurat/GitProcess.scala
@@ -7,11 +7,15 @@ case class GitProcess(gitDir: File) {
   val absPath = gitDir.getAbsolutePath
   final val commitList = "git --git-dir=%s log --reverse --topo-order --no-merges --format=%%H#%%cn#%%cd"
   final val fileFromCommit = "git --git-dir=%s show %s --name-only --oneline"
+  final val contentFile = "git --git-dir=%s show %s:%s"
 
   def filesFromCommits(commitHash: String): Iterator[String] = {
     Process(fileFromCommit.format(absPath, commitHash)).lineStream.to[Iterator].drop(1)
   }
   def commits: Iterator[String] = {
     Process(commitList.format(absPath)).lineStream.to[Iterator]
+  }
+  def fileContent(commitHash: String, file: String): Iterator[String] = {
+    Process(contentFile.format(absPath, commitHash, file)).lineStream.to[Iterator]
   }
 }
--- src/test/scala/ziggurat/GitProcessSpec.scala
+++ src/test/scala/ziggurat/GitProcessSpec.scala
@@ -7,8 +7,9 @@ import org.specs2.Specification
 class GitProcessSpec extends Specification {
   def is =
     s2"""
-          Get commit list from a repository   $commitListFromGitRepo
-          Get a file list from a commit hash  $fileListFromCommit
+          Get commit list from a repository                       $commitListFromGitRepo
+          Get a file list from a commit hash                      $fileListFromCommit
+          Get the content of file from a commit hash and its name $contentFileFromCommit
     """
 
   def commitListFromGitRepo = {
@@ -24,6 +25,15 @@ class GitProcessSpec extends Specification {
     val singleFile = gitProc.filesFromCommits("264ac213c1ecf105e39c6e39aed93358ee0620c6").toList
     val multipleFiles = gitProc.filesFromCommits("e1d460f1ad8a0fdb831e656cfbdb2d7409d89f75").toList
     (singleFile must_== List("README.md")) and (multipleFiles must_== List("andAnotherOne.txt", "anotherfile.txt"))
+  }
+
+  def contentFileFromCommit = {
+    val gitDir: File = new File("./src/test/resources/gitSampleRepo/git")
+    val gitProc = new GitProcess(gitDir)
+    val files = gitProc.filesFromCommits("e1d460f1ad8a0fdb831e656cfbdb2d7409d89f75").toList
+    val content = gitProc.fileContent("e1d460f1ad8a0fdb831e656cfbdb2d7409d89f75", "anotherfile.txt")
+    val contentFromNextCommit = gitProc.fileContent("8688b1463e0e072ba56905bd5cf4c480afd3f918", "anotherfile.txt")
+    (content.mkString("\n") must_== "1") and (contentFromNextCommit.mkString("\n") must_== "1\n2")
   }
 
 }
