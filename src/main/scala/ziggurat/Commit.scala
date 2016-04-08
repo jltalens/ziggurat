a1bdfcb0456b243cf7d6f338f240b6392572f53e
Jose Talens <jose.talens@williamhill.com>
2016-04-08 08:53:57 +0100

    Added the files property to the Commit

--- src/main/scala/ziggurat/Commit.scala
+++ src/main/scala/ziggurat/Commit.scala
@@ -1,5 +1,7 @@
 package ziggurat
 
 import java.util.Date
-case class Commit(id: String, contributor: String = "", date: Date = new Date, files: Iterator[String]) {
+case class Commit(id: String, contributor: String = "", date: Date = new Date, gitProcess: GitProcess) {
+  val files: Iterator[String] = gitProcess.filesFromCommits(id)
+  
 }
